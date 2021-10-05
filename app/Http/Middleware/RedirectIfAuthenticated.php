9126216b89b1bff2df5d6b909de9d5224c438dd1
Eng-IslamMoh <ecapoo2017@gmail.com>
2021-10-05 02:08:29 +0200

    Building an authentication system stage one

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,12 +19,12 @@ class RedirectIfAuthenticated
      */
     public function handle(Request $request, Closure $next, ...$guards)
     {
-        $guards = empty($guards) ? [null] : $guards;
+        if (auth('web')->check()) {
+            return redirect(RouteServiceProvider::HOME);
+        }
 
-        foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
-            }
+        if (auth('admin')->check()) {
+            return redirect(RouteServiceProvider::ADMIN);
         }
 
         return $next($request);
