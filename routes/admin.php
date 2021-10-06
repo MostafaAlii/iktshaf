53ee621ba09b3c17b2f635621dd65c23c0af16bd
Mohammed Mostafa <tayar.shaeby@gmail.com>
2021-10-06 14:29:00 +0200

    update rout admin and user

--- routes/admin.php
+++ routes/admin.php
@@ -14,11 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [DashboardController::class, 'index'])
+Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth:admin'])
     ->name('dashboardAdmin');
 
-Route::get('/testAdminDashboard', function ()
-{
-    return view('admin.layouts.common.master');
-});
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Route::get('/', function () {
 
 require __DIR__.'/auth.php';
 
-//Route::get('/dashboard', function () {
-//    return view('dashboard');
-//})->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', function () {
+  return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
