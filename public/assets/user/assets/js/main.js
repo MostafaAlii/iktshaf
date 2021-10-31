fe8bdab7f7a9ac88a7f3fa0823c4c685bc9d5617
Eng-IslamMoh <ecapoo2017@gmail.com>
2021-10-31 18:21:46 +0200

    fixed1

--- public/assets/user/assets/js/main.js
+++ public/assets/user/assets/js/main.js
@@ -1,4 +1,4 @@
-// Loader
+// Loader 
 $(window).bind('load', function() {
     $('.loader').addClass('dec-opcity');
     setTimeout(function(){
@@ -21,12 +21,14 @@ function topFunction() {
 }
 // Scroll To Top End
 
+
+// Code Commented By Mohamed Mosatafa
 // // Forms Validations
 // (function () {
 //     'use strict'
 //     // Fetch all the forms we want to apply custom Bootstrap validation styles to
 //     var forms = document.querySelectorAll('.needs-validation')
-//
+  
 //     // Loop over them and prevent submission
 //     Array.prototype.slice.call(forms)
 //     .forEach(function (form) {
@@ -41,7 +43,7 @@ function topFunction() {
 //                 console.log("المدخلات  صحيحة ");
 //                 event.stopPropagation()
 //             }
-//
+    
 //             event.preventDefault();
 //             form.classList.add('was-validated')
 //         }, false)
