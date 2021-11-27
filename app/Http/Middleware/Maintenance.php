05fb76909fe9acdf8ea83d8a089e778c94ffa647
Mohammed Mostafa <tayar.shaeby@gmail.com>
2021-11-28 01:13:50 +0200

    fix

--- app/Http/Middleware/Maintenance.php
+++ app/Http/Middleware/Maintenance.php
@@ -1,21 +1,23 @@
 <?php
+
 namespace App\Http\Middleware;
-use App\Models\Setting;
+
 use Closure;
-use Illuminate\Http\Request;
+
 class Maintenance
 {
-    public function handle(Request $request, Closure $next)
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
     {
-        $allSettingsData = Setting::all();
-        $setting['setting'] = $allSettingsData->flatMap(function($allSettingsData) {
-            return [$allSettingsData->key => $allSettingsData->value];
-        });
-        //dd($setting);
-        /*if($setting['site_status'] == '0'){
-            return redirect()->route('maintenanceMode');
-        } else{*/
-            return $next($request);
-        //}
+        if ( setting()->site_status == 0 ) {
+       return redirect('maintenance');
+        }
+        return $next($request);
     }
 }
