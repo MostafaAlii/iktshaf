b31fecdd8f2d8da891777a8a767ef2bc918b83b9
Eng-IslamMoh <ecapoo2017@gmail.com>
2021-12-14 02:31:51 +0200

    add new migrations

--- database/migrations/2021_11_20_130914_create_patterns_table.php
+++ database/migrations/2021_11_20_130914_create_patterns_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTestsTable extends Migration
+class CreatePatternsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreateTestsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tests', function (Blueprint $table) {
+        Schema::create('patterns', function (Blueprint $table) {
             $table->id();
-            $table->string('test');
-            $table->string('passing')->nullable();
-            $table->foreignId('pattern_id')->constrained('patterns')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -29,6 +27,6 @@ class CreateTestsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tests');
+        Schema::dropIfExists('patterns');
     }
 }
